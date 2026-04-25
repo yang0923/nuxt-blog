822f9651bafbf726575d9131502fae11793129e8
yy <yy1875737985@outlook.com>
2026-04-25 12:16:19 +0800

    refactor(app): 移除NuxtPage的keepalive配置

--- app/app.vue
+++ app/app.vue
@@ -13,7 +13,7 @@ router.beforeEach((to, from) => {
 	<div>
 		<NuxtLoadingIndicator color="#3b82f6" :height="3" />
 		<NuxtLayout>
-			<NuxtPage :keepalive="{ max: 10 }"/>
+			<NuxtPage />
 		</NuxtLayout>
 	</div>
 </template>
